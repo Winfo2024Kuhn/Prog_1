01d9647d16ed924b9a0e14362a24dac035265e81
Winfo2024Kuhn <marvin.kuhn@study.thws.de>
2025-01-09 23:35:11 +0100

    Baum Lektion 12 insert Methode hinzu gefügt

--- Übungen/Übungen/src/Lektion_12/Binaerbaum/Baum.java
+++ Übungen/Übungen/src/Lektion_12/Binaerbaum/Baum.java
@@ -15,7 +15,19 @@ public class Baum {
 
     private void insert(Knoten temp, Knoten k) {
         if (temp.wort.compareTo(k.wort) < 0) {
-            if (temp.left =)
+            if (temp.left == null){
+                temp.left = k;
+            }
+            else insert(temp.left,k);
         }
+        else {
+            if (temp.right == null){
+                temp.right = k;
+            }
+            else insert(temp.right,k);
+        }
+    }
+    public void print(){
+
     }
 }
