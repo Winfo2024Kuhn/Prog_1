525715e0f7b99ef40704ab6d51b73513a60ef19a
Winfo2024Kuhn <marvin.kuhn@study.thws.de>
2025-01-15 11:08:49 +0100

    Flasche Klausuraufgabe erstellt bräuchte vergessen fuellstand zu änder zeile 26

--- Übungen/Übungen/src/Altklausuren/Flasche/Flasche.java
+++ Übungen/Übungen/src/Altklausuren/Flasche/Flasche.java
@@ -4,41 +4,44 @@ public class Flasche {
     String name;
     String text;
     int kapazität;
-    int füllstand;
+    int fuellstand;
 
     public Flasche(String name, String text, int kapazität) {
         this.name = name;
         this.text = text;
-
-        if (kapazität > 0) {
-            this.kapazität = kapazität;
-            this.füllstand = kapazität;
+        if (kapazität <= 0) {
+            throw new RuntimeException("Der Füllstand musss größer 0 sein!");
         } else {
-            throw new RuntimeException("Der Füllstand muss positiv sei!");
+            this.kapazität = kapazität;
+            this.fuellstand = kapazität;
         }
     }
 
-    public int reduziereFuellstand(int reduzierendeMenge) {
-        if (füllstand > reduzierendeMenge) {
-            füllstand -= reduzierendeMenge;
-            return reduzierendeMenge;
-        } else{
-            int temp = füllstand;
-            füllstand = 0;
+    public int reduziereFuellstand(int redMenge) {
+        if (fuellstand - redMenge <= 0) {
+            int temp = fuellstand;
+            fuellstand = 0;
             return temp;
+        } else {
+            fuellstand -= redMenge;
+            return redMenge;
         }
     }
 
     public boolean istLeer() {
-        return füllstand == 0;
+        if (fuellstand == 0) {
+            return true;
+        } else return false;
     }
 
     public static void main(String[] args) {
         Flasche f = new Flasche("Prog 1", "SoSe 23", 750);
         while (!f.istLeer()) {
             int i = f.reduziereFuellstand(130);
-            System.out.println("Die entnommene Menge ist: " + i);
+            System.out.println("Es konnten " + i + " ml entnomm	en werden.");
+            System.out.println("Der neue Füllstand beträgt: " + f.fuellstand);
         }
     }
 }
+
 
